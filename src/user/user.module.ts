8c1ec6abbcf4534b2684515e13a8f753f6ace8b3
신승훈 <zlzlzlmo@daum.net>
2022-06-04 08:48:20 +0900

    auth service 생성 후 user service di

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
+// DI를 위해서 providers에 주입할 클래스를 넣어줘야한다.
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  providers: [UserService, AuthService],
   controllers: [UserController],
 })
 export class AuthModule {}
