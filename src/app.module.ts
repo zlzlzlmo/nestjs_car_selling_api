f95e58f62c0abba827100d635619a4b0310840d3
신승훈 <zlzlzlmo@daum.net>
2022-06-05 08:46:58 +0900

    dot-evn, cross-env 설정하여 dev모드 test모드 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,36 @@ import { AuthModule } from './user/user.module';
 import { Report } from './reports/reports.entity';
 import { ReportsModule } from './reports/reports.module';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
   // type orm이 sqlite를 db.sqlite라는 파일 이름으로 root 경로에 만들어줌
   // synchronize 는 어플리케이션이 작동할때 스키마가 자동으로 만들어지게 할것인지 결정함 (엔티티들이 데이터베이세 맞게 동기화가 된다)
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
     AuthModule,
     ReportsModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [
