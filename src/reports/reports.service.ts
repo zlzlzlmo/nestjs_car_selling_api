38854fa06d1d1afd30cb68095d8a724e379d5489
신승훈 <zlzlzlmo@daum.net>
2022-06-10 06:57:22 +0900

    쿼리빌더 적용

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { User } from 'user/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -11,6 +12,18 @@ export class ReportsService {
   constructor(
     @InjectRepository(Report) private readonly repo: Repository<Report>,
   ) {}
+
+  createEstimate({ make, model, lng, lat, year }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('*')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .getRawMany();
+  }
 
   async create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
